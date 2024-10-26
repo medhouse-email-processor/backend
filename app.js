8565e6fa1d1018a0c49c0f32104303365f79e733
Alikhan Baidussenov <a.baydusenov@mail.ru>
2024-10-26 20:27:00 +0000

    Added files download functionality

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const emailRoutes = require('./routes/emailRoutes')
 const driveRoutes = require('./routes/driveRoutes')
 const adminRoutes = require('./routes/adminRoutes')
 const db = require('./config/db') // MySQL connection
-const fs = require('fs')
 const path = require('path')
 
 dotenv.config()
@@ -12,7 +11,6 @@ dotenv.config()
 const app = express()
 
 // Middleware to parse JSON and URL-encoded data
-// i.e. saves in req.body data of request body*
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -22,6 +20,9 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept")
     next()
 })
+
+// Serve static files from the 'public/downloads' directory
+app.use('/downloads', express.static(path.join(__dirname, 'public', 'downloads')))
 
 // Route handlers
 app.use('/api/email', emailRoutes)
--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const archiver = require('archiver')
 const Sender = require('../models/senderModel')
 const { ImapFlow } = require('imapflow')
 const moment = require('moment')
@@ -10,9 +11,10 @@ require('dotenv').config()
 pino.level = 'silent'
 
 exports.fetchAndDownloadOrders = async (req, res) => {
-    const { senderId, day } = req.body
-
+    console.log('Email Fetch Request detected...')
+    const { senderId, day, download } = req.body
     const targetDate = moment.utc(day, 'YYYY-MM-DD')
+
     if (!targetDate.isValid()) {
         return res.status(400).json({ success: false, message: 'Invalid date format, use YYYY-MM-DD.' })
     }
@@ -23,34 +25,27 @@ exports.fetchAndDownloadOrders = async (req, res) => {
             return res.status(404).json({ success: false, message: 'Sender not found.' })
         }
 
-        const email = process.env.HOST_EMAIL
-        const password = process.env.HOST_PASS
         const client = new ImapFlow({
             host: 'imap.mail.ru',
             port: 993,
             secure: true,
             auth: {
-                user: email,
-                pass: password,
+                user: process.env.HOST_EMAIL,
+                pass: process.env.HOST_PASS,
             },
             logger: pino,
         })
-		
+
         await client.connect()
         await client.mailboxOpen('INBOX')
 
-        const messages = await client.search({
-            from: sender.email,
-            on: targetDate.toDate(),
-        })
-
+        const messages = await client.search({ from: sender.email, on: targetDate.toDate() })
         const fetchedFiles = []
         const emailList = []
 
         const mainFolderName = `${sender.companyName}_${day}`
         const mainFolderPath = path.join(__dirname, '../downloads', mainFolderName)
         ensureDownloadDirExists(mainFolderPath)
-
         const cities = sender.cities
 
         for (let uid of messages) {
@@ -58,13 +53,12 @@ exports.fetchAndDownloadOrders = async (req, res) => {
 
             const downloadAttachments = async (parts, uid) => {
                 if (!parts) return []
-
                 const downloadedFiles = []
+
                 for (const part of parts) {
                     const isExcel = part.dispositionParameters?.filename.endsWith('.xlsx') || part.dispositionParameters?.filename.endsWith('.xls')
                     if (part.disposition === 'attachment' && isExcel) {
                         const attachmentFilename = part.dispositionParameters.filename
-
                         const { content } = await client.download(uid, part.part)
 
                         const chunks = []
@@ -75,21 +69,9 @@ exports.fetchAndDownloadOrders = async (req, res) => {
                         const fileType = attachmentFilename.endsWith('.xls') ? 'xls' : 'xlsx'
 
                         const checkResult = checkCellForCity(buffer, fileType, 'F4', cities)
-                        let cityFolderPath
+                        let cityFolderPath = checkResult.success ? path.join(mainFolderPath, checkResult.city) : path.join(mainFolderPath, 'city_undefined')
 
-                        if (checkResult.success) {
-                            cityFolderPath = path.join(mainFolderPath, checkResult.city)
-                            fetchedFiles.push({
-                                filename: attachmentFilename,
-                                city: checkResult.city
-                            })
-                        } else {
-                            cityFolderPath = path.join(mainFolderPath, 'city_undefined')
-                            fetchedFiles.push({
-                                filename: attachmentFilename,
-                                city: false
-                            })
-                        }
+                        fetchedFiles.push({ filename: attachmentFilename, city: checkResult.success ? checkResult.city : false })
 
                         if (!fs.existsSync(cityFolderPath)) {
                             fs.mkdirSync(cityFolderPath, { recursive: true })
@@ -97,9 +79,7 @@ exports.fetchAndDownloadOrders = async (req, res) => {
 
                         const savePath = path.join(cityFolderPath, attachmentFilename)
                         fs.writeFileSync(savePath, buffer)
-
                         console.log(`Downloaded attachment: ${attachmentFilename} to folder: ${checkResult.success ? checkResult.city : 'city_undefined'}`)
-
                         downloadedFiles.push(attachmentFilename)
                     }
                 }
@@ -107,7 +87,6 @@ exports.fetchAndDownloadOrders = async (req, res) => {
             }
 
             const attachmentList = await downloadAttachments(message.bodyStructure.childNodes, uid)
-
             emailList.push({
                 emailTitle: message.envelope.subject,
                 emailDate: message.envelope.date,
@@ -116,15 +95,43 @@ exports.fetchAndDownloadOrders = async (req, res) => {
                 attachments: attachmentList,
             })
         }
+
         await client.logout()
+
+        // Ensure the downloads directory exists
+        const downloadsDir = path.join(__dirname, '../public/downloads')
+        if (!fs.existsSync(downloadsDir)) {
+            fs.mkdirSync(downloadsDir, { recursive: true })
+        }
+
+        // If "download" is set to true, zip the folder
+        if (download) {
+            const zipPath = path.join(downloadsDir, `${mainFolderName}.zip`)
+            const output = fs.createWriteStream(zipPath)
+            const archive = archiver('zip', { zlib: { level: 9 } })
+
+            output.on('close', () => console.log(`Archive created: ${archive.pointer()} bytes`))
+            archive.on('error', (err) => console.error('Error creating archive:', err))
+
+            archive.pipe(output)
+            archive.directory(mainFolderPath, false)
+            await archive.finalize()
+
+            return res.json({
+                success: true,
+                messages,
+                fetchedFiles,
+                mainFolderName,
+                downloadUrl: `downloads/${mainFolderName}.zip`,
+                message: `Fetched and downloaded attachments for sender ${sender.email} on ${day}.`,
+            })
+        }
 
         res.json({
             success: true,
-			messages,
+            messages,
             fetchedFiles,
             mainFolderName,
-			sender,
-            tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
             message: `Fetched and downloaded attachments for sender ${sender.email} on ${day}.`,
         })
     } catch (err) {
@@ -144,7 +151,6 @@ const ensureDownloadDirExists = (mainFolderPath) => {
         const files = fs.readdirSync(mainFolderPath)
         for (const file of files) {
             const filePath = path.join(mainFolderPath, file)
-
             if (fs.statSync(filePath).isDirectory()) {
                 fs.rmSync(filePath, { recursive: true, force: true })
             } else {
